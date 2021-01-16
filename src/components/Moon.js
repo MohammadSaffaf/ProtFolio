98772f47a45697491149f1c6cda77f571262ff46
MohammadSaffaf <mhmdsaffha@gmail.com>
2021-01-16 14:23:31 +0100

    ProtFolio

---
+++ src/components/Moon.js
@@ -0,0 +1,16 @@
+import './Componen.css';
+const Moon = () => {
+    return ( 
+        <section id="section-moon">
+            
+            <div id="div-moon">
+            <h3>PROTFOLIO</h3>
+            
+                
+
+            </div>
+        </section>
+     );
+}
+ 
+export default Moon;
---
+++ src/components/Name.js
@@ -0,0 +1,31 @@
+import './Componen.css';
+const Name = () => {
+    
+    return ( 
+        <article id="headPage">
+            <section id="section-side">
+                <h2>Mohammad Saffaf</h2>
+                <article id="article-side">
+                    <div style={{width: '35%',border:' 2px solid rgb(255, 67, 80)'}}></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </article>
+                <div id="sroll">
+                    <p>SCROLLDOWN ––––––––––</p>
+                    
+                </div>
+            </section>
+        <section id="section-name">
+            <h1>Mohammad Saffaf</h1>
+            <p className="line1"></p>
+            <p className="line2"></p>
+            
+            <h4>Web-Developer /</h4>
+            <h4>IT-Servicetechniker ....</h4>
+        </section>
+        </article>
+     );
+}
+ 
+export default Name;
